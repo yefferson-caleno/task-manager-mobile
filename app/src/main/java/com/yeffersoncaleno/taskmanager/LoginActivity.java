561efbbf208d0a8e67cc642987673ab81b253340
yefferson-caleno <yefferson.caleno@o365.unab.edu.co>
2021-12-09 09:57:52 -0500

    Maquetación de inicio de sesión

--- app/src/main/java/com/yeffersoncaleno/taskmanager/LoginActivity.java
+++ app/src/main/java/com/yeffersoncaleno/taskmanager/LoginActivity.java
@@ -2,18 +2,15 @@ package com.yeffersoncaleno.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.view.Window;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, LoginActivity.class));
+        setContentView(R.layout.activity_login);
     }
 }
